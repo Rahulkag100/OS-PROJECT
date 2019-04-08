7b752c74256a0793a77dcfcb3ae34a0932f6f4c8
Rahulkag100 <32638864+Rahulkag100@users.noreply.github.com>
2019-04-08 23:53:31 +0530

    Update sjf.c
    enter arrival time and burst time

--- sjf.c
+++ sjf.c
@@ -44,4 +44,18 @@ float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
 printf("enter the no. of processes: ");
 
 scanf("%d",&n);
+ for(i = 0; i < n; i++)
+
+{
+
+p_process[i].p_no = i+1;
+
+printf("\nEnter Details For P%d process:-\n", p_process[i].p_no);
+printf("Enter Arrival Time: ");
+scanf("%d", &p_process[i].arrival_t );
+printf("Enter Burst Time: ");
+scanf("%d", &p_process[i].burst_t);
+p_process[i].flag = 0;
+b_t = b_t + p_process[i].burst_t;
+}
 }
