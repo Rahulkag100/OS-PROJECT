f3f1e39861dd17b68484a7d239f1b52f9a6f74d3
Rahulkag100 <32638864+Rahulkag100@users.noreply.github.com>
2019-04-16 08:09:40 +0530

    Update sjf.c
    completion of project

--- sjf.c
+++ sjf.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int n;
-struct process
+struct srf
 {
 
-int p_no;
+int p_no; 
 
 int arrival_t,burst_t,ct,wait_t,taround_time,p;
 
 int flag;
- }p_list[100];
+ }p_process[100];
  
 
 void Sorting()
  {
-struct process p;
+struct srf p;
 
 int i, j;
 
@@ -21,17 +21,18 @@ for(i=0;i<n-1;i++)
 {
   for(j=i+1;j<n;j++)
     {
-      if(p_list[i].arrival_t > p_list[j].arrival_t)
+      if(p_process[i].arrival_t > p_process[j].arrival_t)
         {
-          p = p_list[i];
+          p = p_process[i];
 
-          p_list[i] = p_list[j];
+          p_process[i] = p_process[j];
 
-          p_list[j] = p;
+          p_process[j] = p;
         }
      }
   }
 }
+
 int main()
 {
 
@@ -44,7 +45,8 @@ float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
 printf("enter the no. of processes: ");
 
 scanf("%d",&n);
- for(i = 0; i < n; i++)
+
+for(i = 0; i < n; i++)
 
 {
 
@@ -58,4 +60,45 @@ scanf("%d", &p_process[i].burst_t);
 p_process[i].flag = 0;
 b_t = b_t + p_process[i].burst_t;
 }
+Sorting();
+for(int i=0;i<n;i++)
+{
+a[i]=p_process[i].burst_t;
+}
+p_process[9].burst_t = 9999;
+for(t = p_process[0].arrival_t; t <= b_t+1;)
+{
+peak = 9;
+for(i=0;i<n;i++)
+{
+if(p_process[i].arrival_t <= t && p_process[i].burst_t < p_process[peak].burst_t && p_process[i].flag != 1)
+{
+peak = i;
+}
+if(p_process[peak].burst_t==0 && p_process[i].flag != 1)
+{
+p_process[i].flag = 1;
+p_process[peak].ct=t;
+p_process[peak].burst_t=9999;
+printf("P%d completes in %d\n",p_process[i].p_no,p_process[peak].ct);
+}
+}
+t++;
+(p_process[peak].burst_t)--;
+}
+for(i=0;i<n;i++)
+{
+p_process[i].taround_time=(p_process[i].ct)-(p_process[i].arrival_t);
+avg_taround_time=avg_taround_time+p_process[i].taround_time;
+p_process[i].wait_t=((p_process[i].taround_time)-a[i]);
+avg_w_t=avg_w_t+p_process[i].wait_t;
+}
+printf("PNO\tAT\tCT\tTA\tWTt\n");
+for(i=0;i<n;i++)
+{
+printf("P%d\t%d\t%d\t%d\t%d\n",p_process[i].p_no,p_process[i].arrival_t,p_process[i].ct,p_process[i].taround_time
+,p_process[i].wait_t);
+ }
+printf("Average Turn around Time: %f\t\n\n",avg_taround_time);
+printf("Average Waiting Time :\t %f\t\n",avg_w_t);
 }
